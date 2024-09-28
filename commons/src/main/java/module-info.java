7cdc010386565841a3639012c69cd7e6267638c1
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-28 21:49:14 +0200

    Refactorings

---
+++ commons/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module commons {
+    exports pl.training.commons.annotations;
+    exports pl.training.commons.model;
+}
---
+++ payments/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module payments {
+    exports pl.training.payments.ports.input;
+    exports pl.training.payments.ports.output;
+    exports pl.training.payments.application;
+    exports pl.training.payments.domain;
+    requires commons;
+}
